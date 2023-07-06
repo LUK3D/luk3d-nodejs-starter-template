3bd0b86189d43e0727e04c1bfbd40ea58b8b5fdf
Filipe Lukebana <filipelukebana@gmail.com>
2023-07-06 01:19:44 -0300

    Added fail test example

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -3,3 +3,6 @@ import { expect, test } from "vitest";
 test('Sum two numbers', ()=>{
     expect(1+3).toEqual(4);
 });
+test('Sum two numbers that will fail', ()=>{
+    expect(1+3).toEqual(5);
+});
